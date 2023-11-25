0e70f051ea998eba5e64642e0fc29c5aa9fc3999
cintianingsih <cintia.ningsih07@gmail.com>
2023-11-25 15:44:08 +0700

    update filter

--- static/js/hasilAnalisis.js
+++ static/js/hasilAnalisis.js
@@ -1,98 +1,194 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // const ctx = document.getElementById('myChart');
-    // new Chart(ctx, {
-    //   type: 'bar',
-    //   data: {
-    //     labels: ['8 Okt', '12 Okt', '16 Okt', '20 Okt', '24 Okt', '28 Okt', '1 Nov'],
-    //     datasets: [
-    //       {
-    //         label: 'Prabowo',
-    //         data: [0, 0, 0, 0, 444, 0, 0], 
-    //         backgroundColor: ['#E0CA00'],
-    //         borderColor: ['#E0CA00'],
-    //         borderWidth: 1
-    //       },
-    //       {
-    //         label: 'Ganjar',
-    //         data: [0, 0, 0, 0, 444, 0, 0], 
-    //         backgroundColor: ['#1EEEC9'],
-    //         borderColor: ['#1EEEC9'],
-    //         borderWidth: 1
-    //       },
-    //       {
-    //         label: 'Anies',
-    //         data: [0, 0, 0, 0, 444, 0, 0], 
-    //         backgroundColor: ['#DD65E8'],
-    //         borderColor: ['#DD65E8'],
-    //         borderWidth: 1
-    //       }
-    //     ]
-    //   },
-    //   options: {
-    //     scales: {
-    //       y: {
-    //         beginAtZero: true
-    //       }
-    //     }
-    //   }
-    // });
-   
     const ctx2 = document.getElementById('myChart2');
-    fetch('/get_sentiment_data')  
-        .then(response => response.json())
-        .then(data => {
-            const labels = ['Prabowo', 'Ganjar', 'Anies'];
-            
-            // topicId yang di ambil topicId2 
-            const topicId = 2;
-            const dataset = data.find(item => item.topicId === topicId);
+    const allKeywordButton = document.getElementById('allKeyword');
+    const prabowoButton = document.getElementById('prabowo');
+    const ganjarButton = document.getElementById('ganjar');
+    const aniesButton = document.getElementById('anies');
 
-            if (dataset) {
-                const positiveData = [dataset.prabowo.positif, dataset.ganjar.positif, dataset.anies.positif];
-                const negativeData = [dataset.prabowo.negatif, dataset.ganjar.negatif, dataset.anies.negatif];
-                const neutralData = [dataset.prabowo.netral, dataset.ganjar.netral, dataset.anies.netral];
+    let myChart;
 
-                new Chart(ctx2, {
-                    type: 'bar',
-                    data: {
-                        labels: labels,
-                        datasets: [
-                            {
-                                label: 'Positif',
-                                data: positiveData,
-                                backgroundColor: ['#00E096'],
-                                borderColor: ['#00E096'],
-                                borderWidth: 1
-                            },
-                            {
-                                label: 'Negatif',
-                                data: negativeData,
-                                backgroundColor: ['#EE1E1E'],
-                                borderColor: ['#EE1E1E'],
-                                borderWidth: 1
-                            },
-                            {
-                                label: 'Netral',
-                                data: neutralData,
-                                backgroundColor: ['#979797'],
-                                borderColor: ['#979797'],
-                                borderWidth: 1
-                            }
-                        ]
-                    },
-                    options: {
-                        scales: {
-                            y: {
-                                beginAtZero: true
+    function destroyChart() {
+        if (myChart) {
+            myChart.destroy();
+        }
+    }
+
+    function fetchDataAndRender(topicId, labels, dataCallback) {
+        destroyChart();
+
+        fetch('/get_sentiment_data')
+            .then(response => response.json())
+            .then(data => {
+                const dataset = data.find(item => item.topicId === topicId);
+
+                if (dataset) {
+                    const chartData = dataCallback(dataset);
+
+                    myChart = new Chart(ctx2, {
+                        type: 'bar',
+                        data: {
+                            labels: labels,
+                            datasets: chartData
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
                             }
                         }
-                    }
-                });
-            } else {
-                console.error('Dataset not found for topicId:', topicId);
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
+                    });
+                } else {
+                    console.error('Dataset not found for topicId:', topicId);
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }
+
+    function renderAllKeywordChart() {
+        const labels = ['Prabowo', 'Ganjar', 'Anies'];
+        const topicId = 2;
+
+        fetchDataAndRender(topicId, labels, (dataset) => {
+            const positiveData = [dataset.prabowo.positif, dataset.ganjar.positif, dataset.anies.positif];
+            const negativeData = [dataset.prabowo.negatif, dataset.ganjar.negatif, dataset.anies.negatif];
+            const neutralData = [dataset.prabowo.netral, dataset.ganjar.netral, dataset.anies.netral];
+
+            return [
+                {
+                    label: 'Positif',
+                    data: positiveData,
+                    backgroundColor: ['#00E096'],
+                    borderColor: ['#00E096'],
+                    borderWidth: 1
+                },
+                {
+                    label: 'Negatif',
+                    data: negativeData,
+                    backgroundColor: ['#EE1E1E'],
+                    borderColor: ['#EE1E1E'],
+                    borderWidth: 1
+                },
+                {
+                    label: 'Netral',
+                    data: neutralData,
+                    backgroundColor: ['#979797'],
+                    borderColor: ['#979797'],
+                    borderWidth: 1
+                }
+            ];
         });
+    }
+
+    //default all keywoard
+    renderAllKeywordChart();
+
+    allKeywordButton.addEventListener('click', renderAllKeywordChart);
+
+    prabowoButton.addEventListener('click', function() {
+        const labels = ['Prabowo'];
+        const topicId = 2;
+
+        fetchDataAndRender(topicId, labels, (dataset) => {
+            const positive = [dataset.prabowo.positif];
+            const negative = [dataset.prabowo.negatif];
+            const netral = [dataset.prabowo.netral];
+
+            return [
+                {
+                    label: 'Positif',
+                    data: positive,
+                    backgroundColor: ['#00E096'],
+                    borderColor: ['#00E096'],
+                    borderWidth: 1
+                },
+                {
+                    label: 'Negatif',
+                    data: negative,
+                    backgroundColor: ['#EE1E1E'],
+                    borderColor: ['#EE1E1E'],
+                    borderWidth: 1
+                },
+                {
+                    label: 'Netral',
+                    data: netral,
+                    backgroundColor: ['#979797'],
+                    borderColor: ['#979797'],
+                    borderWidth: 1
+                }
+            ];
+        });
+    });
+
+    ganjarButton.addEventListener('click', function() {
+        const labels = ['Ganjar'];
+        const topicId = 2;
+
+        fetchDataAndRender(topicId, labels, (dataset) => {
+            const positive = [dataset.ganjar.positif];
+            const negative = [dataset.ganjar.negatif];
+            const netral = [dataset.ganjar.netral];
+
+            return [
+                {
+                    label: 'Positif',
+                    data: positive,
+                    backgroundColor: ['#00E096'],
+                    borderColor: ['#00E096'],
+                    borderWidth: 1
+                },
+                {
+                    label: 'Negatif',
+                    data: negative,
+                    backgroundColor: ['#EE1E1E'],
+                    borderColor: ['#EE1E1E'],
+                    borderWidth: 1
+                },
+                {
+                    label: 'Netral',
+                    data: netral,
+                    backgroundColor: ['#979797'],
+                    borderColor: ['#979797'],
+                    borderWidth: 1
+                }
+            ];
+        });
+    });
+
+    aniesButton.addEventListener('click', function() {
+        const labels = ['Anies'];
+        const topicId = 2;
+
+        fetchDataAndRender(topicId, labels, (dataset) => {
+            const positive = [dataset.anies.positif];
+            const negative = [dataset.anies.negatif];
+            const netral = [dataset.anies.netral];
+
+            return [
+                {
+                    label: 'Positif',
+                    data: positive,
+                    backgroundColor: ['#00E096'],
+                    borderColor: ['#00E096'],
+                    borderWidth: 1
+                },
+                {
+                    label: 'Negatif',
+                    data: negative,
+                    backgroundColor: ['#EE1E1E'],
+                    borderColor: ['#EE1E1E'],
+                    borderWidth: 1
+                },
+                {
+                    label: 'Netral',
+                    data: netral,
+                    backgroundColor: ['#979797'],
+                    borderColor: ['#979797'],
+                    borderWidth: 1
+                }
+            ];
+        });
+    });
 });
